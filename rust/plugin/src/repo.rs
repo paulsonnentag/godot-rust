39013c7cea5c26da7f0687705f37181e074d0e79
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-11-28 14:44:10 +0100

    add get_value

--- rust/plugin/src/repo.rs
+++ rust/plugin/src/repo.rs
@@ -1,23 +1,24 @@
-use std::{thread::panicking, time::Duration};
+use std::{str::FromStr, thread::panicking, time::Duration};
 
-use automerge::transaction::Transactable;
+use automerge::{transaction::Transactable, ReadDoc, ScalarValue, Value};
 use godot::classes::INode;
 use godot::global::godot_print;
 use godot::prelude::*;
 
-use automerge_repo::{tokio::FsStorage, ConnDirection, Repo, RepoHandle};
-use tokio::net::TcpStream;
+use automerge_repo::{tokio::FsStorage, ConnDirection, DocumentId, Repo, RepoHandle};
+use tokio::{net::TcpStream, runtime::Runtime};
 
 #[derive(GodotClass)]
 #[class(no_init, base=Node)]
 pub struct AutomergeRepo {
     repo_handle: RepoHandle,
+    runtime: Runtime,
 }
 
 #[godot_api]
 impl AutomergeRepo {
     #[func]
-    fn run() -> Gd<Self> {
+    fn create() -> Gd<Self> {
         let runtime = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .build()
@@ -83,6 +84,42 @@ impl AutomergeRepo {
 
         std::thread::sleep(Duration::from_secs(1));
 
-        return Gd::from_init_fn(|base| Self { repo_handle });
+        return Gd::from_init_fn(|base| Self {
+            repo_handle,
+            runtime,
+        });
+    }
+
+    #[func]
+    fn get_value(&self, doc_id_string: String) {
+        let repo_handle = self.repo_handle.clone();
+        self.runtime.spawn(async move {
+            let doc_id = DocumentId::from_str(&doc_id_string).unwrap();
+            let doc_handle = repo_handle.request_document(doc_id);
+
+            let result = doc_handle.await.unwrap();
+
+            result.with_doc(|d| {
+                let value: i64 = d
+                    .get(automerge::ROOT, "counter")
+                    .unwrap()
+                    .map(|val| match val {
+                        (Value::Scalar(val), automerge::ObjId::Id(_, actor_id, _)) => {
+                            match val.as_ref() {
+                                ScalarValue::Int(num) => *num,
+                                _ => panic!("not a number"),
+                            }
+                        }
+                        _ => panic!("not a number"),
+                    })
+                    .unwrap_or_default();
+
+                println!("value {:?}", value);
+
+                return;
+            });
+        });
+
+        std::thread::sleep(Duration::from_secs(1));
     }
 }
